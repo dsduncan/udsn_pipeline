f4fb1dc6c5a01a609f1534a79d00aa77bdec679d
dsduncan <clonedgoodness@gmail.com>
2020-04-18 17:37:04 -0500

    feat: Add potential for scoring features

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -21,16 +21,26 @@ print(f"Messages data loaded with shape: {messages.shape}")
 c_dupes = categories.id.duplicated()
 categories.drop(categories.index[c_dupes], inplace=True)
 categories.set_index('id', inplace=True)
-print(f"Category data loaded with shape: {messages.shape}")
+print(f"Category data loaded with shape: {categories.shape}")
+
+# Make feature for translated messages
+eng_msg = (messages.message == messages.original) | (messages.original.isna())
+eng_msg = [0 if val else 1 for val in eng_msg]
+messages['translated'] = eng_msg
 
 # Merge data
 df = messages.merge(categories, left_index=True, right_index=True)
 
-# Split message categories
+# Split message categories (convert into integers)
 cat_cols = [col[:-2] for col in df.loc[2, 'categories'].split(';')]
-cat_str = df.categories.replace(r'[^01;]', '', regex=True)
-cat_vals = cat_str.str.split(';', expand=True)
+cat_str = df.categories.replace(r'[^012;]', '', regex=True)
+cat_vals = cat_str.str.split(';', expand=True).astype('int')
 cat_vals.columns = cat_cols
+# Remove labels with no instances
+for col in cat_vals.columns:
+    if cat_vals[col].sum() == 0:
+        print(f"Dropping {col} because it has no instances")
+        cat_vals.drop(columns=col, inplace=True)
 
 # Join message categories to table
 df = pd.concat([df, cat_vals], axis=1, sort=False)
